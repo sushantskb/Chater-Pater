cc3ae18e1ad90a9dbcf0056bc84f7e22a823c1fc
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-06-29 02:02:14 +0530

    signup added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
 
 
 function App() {
 
   return (
     <>
-      <Login />
+      <Signup />
     </>
   );
 }
