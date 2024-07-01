ec553d07e2b2997528bc437db047d61104f87e72
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-07-01 11:57:53 +0530

    added the get user api

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 6969;
-
 
 app.get("/", (req, res) => {
   res.send("<h1>App is healthy</h1>");
@@ -18,9 +18,10 @@ app.get("/", (req, res) => {
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
-    connectToMongoDB()
+  connectToMongoDB();
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
