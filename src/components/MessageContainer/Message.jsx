a91b5cfccec047b1b35c521d3bc37465b5157470
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-07-02 15:01:55 +0530

    added the send functionality

--- src/components/MessageContainer/Message.jsx
+++ src/components/MessageContainer/Message.jsx
@@ -1,17 +1,29 @@
+/* eslint-disable react/prop-types */
+import { useAuthContext } from "../../context/AuthContext";
+import useConversation from "../../zustand/useConversation.js";
 
-const Message = () => {
-
+const Message = ({ message }) => {
+  const { authUser } = useAuthContext();
+  const { selectedConversation } = useConversation();
+  const fromMe = message.senderId === authUser._id;
+  const chatClassName = fromMe ? "justify-end" : "justify-start";
+  const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic
+  const chatColor = fromMe ? "bg-gradient-to-r p-2 rounded-lg from-blue-500 to-purple-400 text-white max-w-xs"  : "bg-gradient-to-r p-2 rounded-lg from-yellow-500 to-green-400 text-white max-w-xs"
   return (
-    <div className="flex justify-end">
-      <div className="bg-gradient-to-r p-2 rounded-lg from-blue-500 to-purple-400 text-white max-w-xs">
-        Hello!!👋
+    <div className={`flex ${chatClassName}`}>
+      <div className="chat-image avatar">
+        <div className="w-10 rounded-full">
+          <img src={profilePic ? profilePic : "./assets/person.png"} />
+        </div>
+      </div>
+      <div className={`${chatColor}`}>
+        {message.message}
       </div>
     </div>
   );
 };
 
 export default Message;
-
 
 // `flex ${isUser ? "justify-end" : "justify-start"}`
 // `p-2 rounded-lg ${isUser ? "bg-blue-500 text-white" : "bg-gray-700 text-white"} max-w-xs`
--- src/components/MessageContainer/Messages.jsx
+++ src/components/MessageContainer/Messages.jsx
@@ -1,16 +1,33 @@
+import { useEffect, useRef } from "react";
+import useGetMessages from "../../hooks/useGetMessages";
 import Message from "./Message";
 
 const Messages = () => {
+  const { messages, loading } = useGetMessages();
+  const lastMessageRef = useRef();
 
+  useEffect(() => {
+    setTimeout(() => {
+      lastMessageRef.current?.scrollIntoView({ behavior: "smooth" }, 100);
+    });
+  }, [messages]);
   return (
     <div className="flex flex-col space-y-4 p-4 bg-gray-800 rounded-lg">
-      {/* {messages.map(message => (
-        <Message key={message.id} message={message} />
-      ))} */}
-      <Message />
-      <Message />
-      <Message />
-      <Message />
+      {
+        !loading && 
+        messages.length > 0 && 
+        messages.map((message) => (
+          <div key={message._id} ref={lastMessageRef}>
+            <Message message={message} />
+          </div>
+        ))
+      }
+
+      {
+        !loading && messages.length === 0 && (
+          <p className="text-center">Send a message to start the conversation</p>
+        )
+      }
     </div>
   );
 };
