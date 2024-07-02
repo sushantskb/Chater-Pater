d0c076818edac64cce69b5013aab4aba7eda4037
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-07-02 20:31:17 +0530

    added the client side socket io and done handshake between the client and server and fetched the online users using the socket io

--- src/components/MessageContainer/MessageContainer.jsx
+++ src/components/MessageContainer/MessageContainer.jsx
@@ -17,7 +17,7 @@ const MessageContainer = () => {
       ) : (
         <div className="backgroundImage flex flex-col h-full w-full sm:w-3/4 bg-gray-900 text-white p-4">
           <div className="flex justify-between items-center mb-4">
-            <div className="text-2xl font-bold">Test User</div>
+            <div className="text-2xl font-bold">{selectedConversation.fullName}</div>
           </div>
           <div className="flex-grow overflow-y-auto">
             <Messages />
--- src/components/SideBar/Conversation.jsx
+++ src/components/SideBar/Conversation.jsx
@@ -1,15 +1,18 @@
 /* eslint-disable react/prop-types */
 
+import { useSocketContext } from "../../context/SocketContext";
 import useConversation from "../../zustand/useConversation";
 
 const Conversation = ({ conversation, lastIndex }) => {
   const { selectedConversation, setSelectedConversation } = useConversation();
 
   const isSelected = selectedConversation?._id === conversation._id;
+  const { onlineUsers } = useSocketContext();
+  const isOnline = onlineUsers.includes(conversation._id);
   return (
     <>
       <div
-        className={`online flex items-center p-2 hover:bg-gray-700 rounded-lg cursor-pointer relative ${
+        className={`flex items-center p-2 hover:bg-gray-700 rounded-lg cursor-pointer relative ${
           isSelected ? "bg-gray-700" : ""
         }`}
         onClick={() => setSelectedConversation(conversation)}
@@ -24,10 +27,13 @@ const Conversation = ({ conversation, lastIndex }) => {
           className="h-10 w-10 rounded-full mr-2"
         />
         <span className="text-white">{conversation.fullName}</span>
-        <span className="absolute right-0 h-3 w-3 bg-green-500 rounded-full border-2 border-gray-800"></span>
+        {isOnline ? (
+          <span className="absolute right-0 h-3 w-3 bg-green-500 rounded-full border-2 border-gray-800"></span>
+        ) : (
+          ""
+        )}
       </div>
       {!lastIndex ? <div className="divider my-0 py-0 h-1" /> : ""}
-
     </>
   );
 };
