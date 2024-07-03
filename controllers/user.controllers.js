a234d5d532c8e980731b7583ecaaa0e37c5af7e5
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-07-03 14:51:20 +0530

    added get users api for group creation

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -3,11 +3,23 @@ import User from "../models/user.model.js";
 export const getUsersForSidebar = async (req, res) => {
   try {
     const loggedInUser = req.user._id;
-    const filteredUsers = await User.find({ _id: { $ne: loggedInUser } }).select("-password");
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUser },
+    }).select("-password");
 
     return res.status(200).json(filteredUsers);
   } catch (error) {
     console.log("Error in get user for slidebar", error);
     res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const getUsersForGroupCreation = async (req, res) => {
+  try {
+    const users = await User.find().select("_id username");
+    res.status(200).json(users);
+  } catch (error) {
+    console.error("Error fetching users: ", error);
+    res.status(500).json({error: "Internal server error"})
   }
 };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,13 @@
 import express from "express";
 import protectRoute from "../middleware/protectRoute.js";
-import { getUsersForSidebar } from "../controllers/user.controllers.js";
+import {
+  getUsersForGroupCreation,
+  getUsersForSidebar,
+} from "../controllers/user.controllers.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, getUsersForSidebar);
+router.get("/users", protectRoute, getUsersForGroupCreation);
 
 export default router;
