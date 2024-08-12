d4076b26b75f441860aa9c3b4adf74f29a1a25bd
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-08-12 09:24:01 +0530

    *

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -73,7 +73,7 @@ export const logout = async (req, res) => {
     res.cookie("jwt", "", { maxAge: 0 });
     res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
-    console.log("Error in logout controller", error.message);
+    console.log("Error in logout controller", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
