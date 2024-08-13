8a6d2995807c6403ec6208be907d9226b0e43f43
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-08-13 16:45:39 +0530

    changed the hooks and production deploy changes done

--- server/app.js
+++ server/app.js
@@ -5,22 +5,14 @@ import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import groupMessageRouter from "./routes/group.routes.js";
+import path from "path";
 import connectToMongoDB from "./db/connectToMongoDB.js";
-import axios from "axios";
 
-import {app, server} from "./socket/socket.js"
+import { app, server } from "./socket/socket.js";
 dotenv.config();
 const PORT = process.env.PORT || 6969;
 
-app.get("/", (req, res) => {
-  res.send("<h1>App is healthy</h1>");
-});
-
-// keep alive mechanism
-// Create a keep-alive endpoint
-app.get("/keep-alive", (req, res) => {
-  res.status(200).send("Server is alive");
-});
+const __dirname = path.resolve();
 
 // middlewares
 app.use(express.json());
@@ -30,23 +22,14 @@ app.use("/api/users", userRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/groups", groupMessageRouter);
 
-server.listen(PORT, '0.0.0.0', (err) => {
-  connectToMongoDB();
-  if (err) {
-    console.log("Error in starting Node.js server");
-  } else {
-    console.log(`Server is running on port ${PORT}`);
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
-    // Set up keep-alive ping
-    setInterval(() => {
-      axios
-        .get(`http://localhost:${PORT}/keep-alive`)
-        .then((response) => {
-          console.log("Keep-alive ping successful:", response.data);
-        })
-        .catch((error) => {
-          console.error("Keep-alive ping failed:", error);
-        });
-    }, 300000); // Ping every 5 minutes
-  }
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
+server.listen(PORT, "0.0.0.0", (err) => {
+  connectToMongoDB();
+
+  console.log(`Server is running on port ${PORT}`);
 });
