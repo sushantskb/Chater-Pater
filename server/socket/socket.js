1edd7a9b24f9d044854bc7a64c172916878eb007
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-08-17 23:43:09 +0530

    uri updated

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -7,7 +7,11 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://192.168.2.172:5173"],
+    origin: [
+      "http://localhost:5173",
+      "http://192.168.2.172:5173",
+      "https://chater-pater.onrender.com",
+    ],
     methods: ["GET", "POST"],
   },
 });
