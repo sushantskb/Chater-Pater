100150526ac05f0f27743d6f8e78bbd15f4ca3bd
Sushant Kumar Bishoi <91365425+sushantskb@users.noreply.github.com>
2026-02-23 02:40:06 +0530

    Update socket connection URL for production

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -22,7 +22,7 @@ export const SocketContextProvider = ({ children }) => {
       //   window.location.hostname === "localhost"
       //     ? "http://localhost:9000"
       //     : "https://chater-pater-backend.onrender.com";
-      const socket = io("https://chater-pater.up.railway.app", {
+      const socket = io("https://college-fest-2024-production.up.railway.app", {
         query: {
           userId: authUser._id,
         },
